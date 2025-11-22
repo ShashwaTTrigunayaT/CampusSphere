a17d9ddd8cc4ac947b68402853509ed7ac7444f6
Shashwat  Prasad Trigunayat <shashwattrigunayat04@gmail.com>
2025-11-23 00:18:53 +0530

    Update fetchEvents.js

--- Server/Cron/fetchEvents.js
+++ Server/Cron/fetchEvents.js
@@ -9,7 +9,7 @@ const {sendEmailAlerts} = require("../service/alerts");
 const fetchDevFolioEvents = require("../service/devFolioFetcher");
 const fetchUnstopEvents = require("../service/unstopFetcher");
 
-cron.schedule("*/60 * * * *", async () => { 
+cron.schedule("* * * * *", async () => { 
 
   
   
