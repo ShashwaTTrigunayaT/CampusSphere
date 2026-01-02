a37d40c8646569f82a8f7d26c74be1c7bf8f1a7c
Shashwat  Prasad Trigunayat <shashwattrigunayat04@gmail.com>
2026-01-03 00:00:31 +0530

    Update matchRoutes.js

--- Server/service/matchRoutes.js
+++ Server/service/matchRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const Event = require('../models/Event'); 
+const Event = require('../models/event'); 
 const pdfParse = require('pdf-parse');
 
 const storage = multer.memoryStorage();
